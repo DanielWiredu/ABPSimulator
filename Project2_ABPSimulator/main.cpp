916910c0e85125cfcf3a04b7549b631a54bce865
Daniel Wiredu <danielwiredu@gmail.com>
2023-11-18 00:42:16 -0330

    Initial Commit

---
+++ Project2_ABPSimulator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project2_ABPSimulator
+//
+//  Created by Daniel Wiredu on 11/18/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
